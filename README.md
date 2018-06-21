8c4a00fe8518a41c204839f3c5a3003332c90c4f
Radovan Balog <radovan.balog@2ring.com>
2018-06-21 12:25:09 +0200

    Initial script commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Gadgets-Translation-Crawler
+
+Run the script with `npm start`.
+
+The script will use the *locales* folder in the project root directory.
+It will process all translation files and output a *missing.csv* file with the `key,value` pairs in a table format.
---
+++ index.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const glob = require('glob');
+const sortJson = require('sort-json');
+
+const missingTranslations = new Set();
+let en = {};
+
+fs.readFile('locales/en_us/translation.json', { encoding: 'utf8' }, (err, data) => {
+    if (err) throw err;
+    en = JSON.parse(data.trim());
+
+    glob('locales/**/*.json', (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+            if (file.indexOf('en_us') === -1) {
+                fs.readFile(file, { encoding: 'utf8' }, (err, data) => {
+                    if (err) throw err;
+                    const locale = JSON.parse(data.trim());
+                    processTranslation(en, locale);
+                    fs.writeFile(file, JSON.stringify(locale, null, 4), (err) => {
+                        if (err) throw err;
+                        sortJson.overwrite(file, { ignoreCase: true });
+                    });
+                });
+            }
+        });
+    });
+});
+
+process.on('exit', () => {
+    if (missingTranslations.size) {
+        console.log('Missing translations:');
+        let table = '';
+        missingTranslations.forEach((key) => {
+            console.log('    ' + key);
+            table += key + ',\"' + getValue(key) + '\"\r\n';
+        });
+
+        fs.writeFileSync('missing.csv', table);
+    }
+});
+
+function getValue(key) {
+    let result = en;
+    for (const sub of key.split('.')) {
+        result = result[sub];
+    }
+
+    return result;
+}
+
+function processTranslation(source, target, path = '') {
+    const sourceKeys = Object.keys(source);
+    const targetKeys = Object.keys(target);
+    targetKeys.filter((key) => sourceKeys.find((sourceKey) => sourceKey === key) == null).forEach((key) => delete target[key]);
+
+    sourceKeys.forEach((key) => {
+        if (typeof source[key] === 'string' && typeof target[key] !== 'string') {
+            target[key] = source[key];
+            missingTranslations.add(path + key);
+        } else if (typeof source[key] === 'object') {
+            if (typeof target[key] !== 'object') {
+                target[key] = source[key]
+            } else {
+                processTranslation(source[key], target[key], key + '.');
+            }
+        }
+    });
+}
